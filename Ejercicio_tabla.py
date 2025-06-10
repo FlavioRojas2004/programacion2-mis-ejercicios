2ad81e83ccd70343a56675f007c9d5392ee30e38
Flavio Rojas <rojasvargasflavio9@gmail.com>
2025-06-10 13:34:52 +0000

    actualizado ejemplo de tabla de multiplicar Ejercicio_tabla.py con saludo y nombre

--- Ejercicio_tabla.py
+++ Ejercicio_tabla.py
@@ -1,14 +1,5 @@
-edad = int(input("Por favor, ingresa tu edad: "))
-
-def obtener_mensaje_por_edad(edad):
-    if edad < 0:
-        return "Edad no válida, por favor ingresa un valor positivo."
-    elif edad >= 18:
-        return "¡Puedes ver películas clasificadas R!"
-    elif edad >= 13:
-        return "Puedes ver películas clasificadas PG-13."
-    else:
-        return "Te recomendamos películas clasificadas G o PG."
-
-mensaje_cine = obtener_mensaje_por_edad(edad)
-print (mensaje_cine)
+numero_tabla = int(input("Ingresa un número para ver su tabla de multiplicar que desea realizar: ")) #Pide la insercion de los datos a realizar 
+print(f"--- Tabla del numero {numero_tabla} ---") #
+for i in range(1, 11): # i tomará valores de 1 a 10
+ resultado = numero_tabla * i
+ print(f"{numero_tabla} x {i} = {resultado}") #Muestra el resultado en consola 
