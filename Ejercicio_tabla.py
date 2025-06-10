f4903d4467bac738c6c7e86047bb7fbd5b96216c
Flavio Rojas <rojasvargasflavio9@gmail.com>
2025-06-10 13:28:57 +0000

    Agregando ejemplo de tabla de multiplicar Ejercicio_tabla.py con saludo y nombre

---
+++ Ejercicio_tabla.py
@@ -0,0 +1,14 @@
+edad = int(input("Por favor, ingresa tu edad: "))
+
+def obtener_mensaje_por_edad(edad):
+    if edad < 0:
+        return "Edad no válida, por favor ingresa un valor positivo."
+    elif edad >= 18:
+        return "¡Puedes ver películas clasificadas R!"
+    elif edad >= 13:
+        return "Puedes ver películas clasificadas PG-13."
+    else:
+        return "Te recomendamos películas clasificadas G o PG."
+
+mensaje_cine = obtener_mensaje_por_edad(edad)
+print (mensaje_cine)
