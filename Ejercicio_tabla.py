e1a7e97ab7df7dfba20243876beefb5ba4352060
Flavio Rojas <rojasvargasflavio9@gmail.com>
2025-06-10 10:02:05 -0400

    Delete Ejercicio_tabla.py

--- Ejercicio_tabla.py
+++
@@ -1,5 +0,0 @@
-numero_tabla = int(input("Ingresa un número para ver su tabla de multiplicar que desea realizar: ")) #Pide la insercion de los datos a realizar 
-print(f"--- Tabla del numero {numero_tabla} ---") #
-for i in range(1, 11): # i tomará valores de 1 a 10
- resultado = numero_tabla * i
- print(f"{numero_tabla} x {i} = {resultado}") #Muestra el resultado en consola 
