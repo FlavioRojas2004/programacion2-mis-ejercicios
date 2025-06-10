127e1f32e21151b615ceee98c9b966928aeecb6f
Flavio Rojas <rojasvargasflavio9@gmail.com>
2025-06-10 14:25:58 +0000

    ejercicio bucle_for.py

---
+++ bucle_for.py
@@ -0,0 +1,7 @@
+print("Contando hasta 3 (sin incluirlo):") # Pedir los datos a realizar
+for numero in range(3): # range(3) genera 0, 1, 2 #Dando los valores a la variable
+ print(numero)
+print("\nRecorriendo un string:") 
+nombre = "PYTHON" #valor a la variable
+for letra in nombre: # Recorre cada letra en la cadena "PYTHON"
+ print(letra)
