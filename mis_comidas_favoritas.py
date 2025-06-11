5d56579fc423ea42744de82dbdab1c0c500d69e4
Flavio Rojas <rojasvargasflavio9@gmail.com>
2025-06-11 12:51:01 +0000

    agregando mis_comidas_favoritas.py

---
+++ mis_comidas_favoritas.py
@@ -0,0 +1,9 @@
+# Lista de comidadas favoritas
+comidas = ["Locro", "Picate de Pollo", "Asado en olla"]
+# Mostrar las comidas favoritas
+print("Estos son mis 3 comidas favoritas:")
+print("-----------------------------------")
+for i, comidas in enumerate(comidas, start=1):
+    print(f"Comidas {i}: {comidas}")
+print("-----------------------------------")
+print("Flavio Rojas.")
